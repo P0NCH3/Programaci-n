d9deda44def0c9f5e56d313cd2f6e7dd0c886da3
Maximiliano Alfonso <134652852+P0NCH3@users.noreply.github.com>
2025-02-07 00:42:54 -0600

    Add files via upload

---
+++ venta/eva1_5_tele.java
@@ -0,0 +1,28 @@
+package venta;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+ /**
+ *
+ * @author ririo
+ */
+
+      /**
+       * @param args the command line arguments
+       */
+    public class eva1_5_tele {
+      public static void main(String[] args) {
+          // Crear un objeto de la clase Television
+        Television smartv = new Television();
+        smartv.imprimirDatos();
+        smartv.encenderApagar();
+        smartv.subirVolumen();
+        smartv.bajarVolumen(); 
+        smartv.cambiarCanal();
+               
+        }
+          
+
+}
